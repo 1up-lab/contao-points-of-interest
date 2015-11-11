0ef129e59d3443dfa2955b8528ea62aa32380270
David Greminger <david.greminger@1up.io>
2015-11-11 13:02:17 +0100

    Fixed autoloading

--- contao/config/autoload.php
+++ contao/config/autoload.php
@@ -1,7 +1,5 @@
 <?php
 
-NamespaceClassLoader::add('Oneup', 'system/modules/points-of-interest/src');
-
 $templatesFolder = version_compare(VERSION, '4.0', '>=')
     ? 'vendor/oneup/contao-points-of-interest/templates'
     : 'system/modules/points-of-interest/templates';
