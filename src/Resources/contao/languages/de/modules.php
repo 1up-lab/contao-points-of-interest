cb3e41ee9f9166bd936acaf6e61b937c5a19ca3f
Oliver Schmidt <oliver.schmidt@1up.io>
2023-06-15 09:41:34 +0200

    Adjust translation files

---
+++ src/Resources/contao/languages/de/modules.php
@@ -0,0 +1,6 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['CTE']['points_of_interest'] = ['Points of interest (POI)', 'Erzeugt ein Bild mit klickbaren Info-Punkten (POI).'];
+$GLOBALS['TL_LANG']['MOD']['points_of_interest'] = ['Points of interest', 'Verwalten Sie hier Bilder mit klickbaren Info-Punkten darauf.'];
---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['CTE']['points_of_interest'] = ['Points of interest (POI)', 'Displays an image with clickable points of interest (POI).'];
+$GLOBALS['TL_LANG']['FMD']['points_of_interest'] = ['Points of interest (POI)', 'Displays an image with clickable points of interest (POI).'];
+$GLOBALS['TL_LANG']['MOD']['points_of_interest'] = ['Points of interest', 'Manage images with clickable points of interest.'];
